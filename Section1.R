aec56b30e57593903c03772be45769230ab991de
Avery King <jamesaveryking@gmail.com>
2017-08-31 10:04:15 -0400

    Update Section1.R

--- Section1.R
+++ Section1.R
@@ -1,4 +1,7 @@
 #Data preprocessing part 1 section 2
+
+library(caTools)
+
 dataset<-read.csv('Data.csv')
 
 
@@ -19,9 +22,19 @@ dataset$Salary = ifelse(is.na(dataset$Salary),#condition checked
 dataset$Country = factor(dataset$Country,
                          levels = c('France','Spain','Germany'),
                          labels = c(1,2,3))
-dataset$Purchase = factor(dataset$Purchased,
+dataset$Purchased = factor(dataset$Purchased,
                          levels = c('No','Yes'),
                          labels = c(0,1))
-
 x<-dataset[,1:3]
 y<-dataset[,4]
+
+#creating training and test sets
+set.seed(123)
+
+split = sample.split(dataset$Purchased,SplitRatio=0.8)#as opposed to python this is for training set not testing set like in python
+training_set = subset(dataset,split==TRUE)
+test_set = subset(dataset,split==FALSE)
+
+#scaling numeric variables -- won't work without specifying numeric columns
+training_set[,2:3] = scale(training_set[,2:3])
+test_set[,2:3] = scale(test_set[,2:3])
